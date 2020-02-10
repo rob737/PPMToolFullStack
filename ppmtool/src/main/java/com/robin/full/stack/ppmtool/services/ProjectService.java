05cafa4463d4904c7f479ee783d3651075807edb
rob737 <robinsrivastava737@gmail.com>
2020-02-10 22:45:46 +0530

    Backend crud operations

---
+++ ppmtool/src/main/java/com/robin/full/stack/ppmtool/services/ProjectService.java
@@ -0,0 +1,41 @@
+package com.robin.full.stack.ppmtool.services;
+
+import com.robin.full.stack.ppmtool.domain.Project;
+import com.robin.full.stack.ppmtool.exceptions.ProjectIdException;
+import com.robin.full.stack.ppmtool.repositories.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectService {
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    public Project saveOrUpdateProject(Project project) {
+
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception exception) {
+            throw new ProjectIdException("Project Id '" + project.getProjectIdentifier().toUpperCase() + "' already exists");
+        }
+
+    }
+
+    public Project findProjectByIdentifier(String projectId) {
+
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if (project == null) {
+            throw new ProjectIdException("Project does not exist");
+        }
+        return project;
+    }
+
+    public Iterable<Project> findAllProjects(){
+        return projectRepository.findAll();
+    }
+
+
+
+}
